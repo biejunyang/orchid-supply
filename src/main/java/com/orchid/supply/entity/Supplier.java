1037e051e552f071a02a720c18932d0786a90f0c
e <biejunyang@cngb.org>
2020-05-15 10:47:08 +0800

    ccccccc

--- src/main/java/com/orchid/supply/entity/Supplier.java
+++ src/main/java/com/orchid/supply/entity/Supplier.java
@@ -1,12 +1,9 @@
 package com.orchid.supply.entity;
 
-import java.util.Date;
-import com.baomidou.mybatisplus.extension.activerecord.Model;
-import com.baomidou.mybatisplus.annotation.TableName;
-import com.baomidou.mybatisplus.annotation.TableField;
-import com.baomidou.mybatisplus.annotation.TableId;
-import com.baomidou.mybatisplus.annotation.IdType;
-import java.io.Serializable;
+import com.orchid.mybatis.entity.BaseEntity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * (Supplier)表实体类
@@ -15,106 +12,18 @@ import java.io.Serializable;
  * @since 2020-04-10 17:06:07
  */
 @SuppressWarnings("serial")
-@TableName("supplier")
-public class Supplier extends Model<Supplier> {
+//@TableName("supplier")
 
-    @TableId(value="id")
-    private Long id;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Supplier extends BaseEntity<Supplier> {
 
-    @TableField("name")
     private String name;
 
-    @TableField("phone")
     private String phone;
 
-    @TableField("address")
     private String address;
 
-    @TableField("create_time")
-    private Date createTime;
 
-    @TableField("create_name")
-    private String createName;
-
-    @TableField("update_name")
-    private String updateName;
-
-    @TableField("update_time")
-    private Date updateTime;
-
-
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public Date getCreateTime() {
-        return createTime;
-    }
-
-    public void setCreateTime(Date createTime) {
-        this.createTime = createTime;
-    }
-
-    public String getCreateName() {
-        return createName;
-    }
-
-    public void setCreateName(String createName) {
-        this.createName = createName;
-    }
-
-    public String getUpdateName() {
-        return updateName;
-    }
-
-    public void setUpdateName(String updateName) {
-        this.updateName = updateName;
-    }
-
-    public Date getUpdateTime() {
-        return updateTime;
-    }
-
-    public void setUpdateTime(Date updateTime) {
-        this.updateTime = updateTime;
-    }
-
-    /**
-     * 获取主键值
-     *
-     * @return 主键值
-     */
-    @Override
-    protected Serializable pkVal() {
-        return this.id;
-    }
-    }
+}
